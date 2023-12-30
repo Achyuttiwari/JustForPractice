b324eb7989a4e48248cb18a820d4ccfedcf2377d
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2023-12-30 21:30:04 +0530

    fibonacci without recursion.

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -4,12 +4,11 @@ public class Fibonacci {
 
     static void fibonnaciWithoutRecursion(int count){
         int firstNumber = 1, secondNumber = 1;
-        int count1;
         for(int i = 0; i < count; i++){
             if (i == 0) System.out.print(0 + " ");
             else if (i == 1 || i == 2) System.out.print(1 + " ");
             else {
-                count1 = firstNumber + secondNumber;
+                int count1 = firstNumber + secondNumber;
                 System.out.print(count1 + " ");       //output: 0 1 1 2 3 5 8 13 21 34
                 firstNumber = secondNumber;
                 secondNumber = count1;
