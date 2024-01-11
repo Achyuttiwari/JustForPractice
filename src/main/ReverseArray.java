97a522cb11bb0a97be0fea090c996618733bca00
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-01-11 18:42:03 +0530

    ....

---
+++ src/main/ReverseArray.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner scanner  = new Scanner(System.in);
+        int i = 0;
+        int[] array = new int[scanner.nextInt()];
+        for(i = 0; i < array.length; i++){
+            array[i] = scanner.nextInt();
+        }
+        for(int k: array) {
+            System.out.println("k = " + k);
+                System.out.println(array[array.length - ]);
+            }
+        }
+}
