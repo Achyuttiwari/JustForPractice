c44362a2efe3523a7c005c63f56ef07e0ec761c2
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-01-24 22:15:37 +0530

    ....!!!

--- src/main/ReverseArray.java
+++ src/main/ReverseArray.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class ReverseArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the Array: ");
         int[] array1 = new int[scanner.nextInt()];
+        System.out.println("Enter the value of the array: ");
         for (int i = 0; i < array1.length; i++) {
             array1[i] = scanner.nextInt();
         }
