3f737092bad0834f535b07be18e4c9bced4dd629
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-03-22 22:09:17 +0530

    Sorting in ascending and descending order using insertionsort without inbuilt method.

--- src/insertionSort/InsertionSort.java
+++ src/insertionSort/InsertionSort.java
@@ -1,6 +1,7 @@
 package insertionSort;
 
 import com.sun.security.jgss.GSSUtil;
+import selectionSort.SelectionSort;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -15,14 +16,41 @@ public class InsertionSort {
         for (int i = 0; i < sizeOfArray; i++) {
             array[i] = scanner.nextInt();
         }
+        System.out.println("Enter your choice 1 for Ascending Order \n 2 for Descending Order");
+        int choice = scanner.nextInt();
         try{
             InsertionSort insertionsort = new InsertionSort();
-            insertionsort.arrayInAscendingOrder(array, sizeOfArray);
-            System.out.println(Arrays.toString(array));
+
+            switch (choice) {
+                case 1:
+                    insertionsort.arrayInAscendingOrder(array, sizeOfArray);
+                    System.out.println("Array after soring in Ascending order: " + Arrays.toString(array));
+                    break;
+                case 2:
+                    insertionsort.arrayInDescendingOrder(array, sizeOfArray);
+                    System.out.println("Array after soring in Descending order" + Arrays.toString(array));
+                    break;
+                default:
+                    System.out.println("Regret Wrong choice \nYou only have two choices 1 and 2");
+            }
         }
         catch(Exception exception){
             exception.printStackTrace();
         }
+    }
+
+    private int[] arrayInDescendingOrder(int[] array, int sizeOfArray) {
+        for (int i = 0; i < sizeOfArray; i++) {
+            int keyValue = array[i];
+            int j = i - 1;
+            while(j <= 0 && array[j] < keyValue){
+                array[j + 1] = array[j];
+                j = j - 1;
+
+            }
+            array[j + 1] = keyValue;
+        }
+        return array;
     }
 
     private int[] arrayInAscendingOrder(int[] array, int sizeOfArray) {
