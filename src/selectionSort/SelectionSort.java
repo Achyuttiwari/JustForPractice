b63ce30eeadf2ba870efbe636ef0d66c2e379c1e
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-03-23 20:56:23 +0530

    Sorting in ascending and descending order using selectionsort without inbuilt method.

--- src/selectionSort/SelectionSort.java
+++ src/selectionSort/SelectionSort.java
@@ -15,21 +15,20 @@ public class SelectionSort {
         }
         System.out.println("Array before sorting " + Arrays.toString(array));
         SelectionSort selectionSort = new SelectionSort();
-        System.out.println("Enter your choice 1 for Ascending Order and " +
-                "2 for Descending order");
+        System.out.println("Enter your choice 1 for Ascending Order and 2 for Descending order");
         int choice = scanner.nextInt();
         try {
             switch (choice) {
                 case 1:
                     selectionSort.arrayInAscendingOrder(array, sizeOfArray);
-                    System.out.println("Array after soring in Ascending order: " + Arrays.toString(array));
+                    System.out.println("Array after soring in Ascending order:\t" + Arrays.toString(array));
                     break;
                 case 2:
                     selectionSort.arrayInDescendingOrder(array, sizeOfArray);
-                    System.out.println("Array after soring in Descending order" + Arrays.toString(array));
+                    System.out.println("Array after soring in Descending order:\t" + Arrays.toString(array));
                     break;
                 default:
-                    System.out.println("Regret Wrong choice \nYou only have two choices 1 and 2");
+                    System.out.println("Regret Wrong choice \n You only have two choices 1 and 2");
             }
         }
         catch(Exception se){
